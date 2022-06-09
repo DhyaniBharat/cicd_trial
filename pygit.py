1afd05be5496e966068bd627faf81ca9a72f8009
DhyaniBharat <bharat.dhyani@indiumsoft.com>
2022-06-09 14:02:59 +0530

    commmit first

---
+++ pygit.py
@@ -0,0 +1,8 @@
+from git import *
+
+# rorepo is a Repo instance pointing to the git-python repository.
+# For all you know, the first argument to Repo is a path to the repository
+# you want to work with
+repo = Repo("D:\pygit_trial\cicd_trial")
+print(repo)
+
